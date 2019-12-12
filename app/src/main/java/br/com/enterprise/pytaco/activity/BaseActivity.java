0d6ea18f78b3e498d24b7b59eb834269ee668efb
Ronie <rsm.ronie@gmail.com>
2019-12-12 00:15:35 -0300

    Alteração de EditText para TextInputLayout na tela de login

--- app/src/main/java/br/com/enterprise/pytaco/activity/BaseActivity.java
+++ app/src/main/java/br/com/enterprise/pytaco/activity/BaseActivity.java
@@ -72,4 +72,15 @@ public class BaseActivity extends FragmentActivity {
     protected void pEnableScreen() {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
     }
+
+    protected void pHideKeboard(){
+        InputMethodManager imm = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = this.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(this);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
 }
